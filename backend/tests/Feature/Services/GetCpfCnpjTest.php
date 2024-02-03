a9a3ed4ab2202452829c981b2b376728b64c10f2
Ivo <ivo_vilar@hotmail.com>
2024-02-03 14:56:26 -0300

    backend: should be able to return 400 response for invalid cnpj

--- backend/tests/Feature/Services/GetCpfCnpjTest.php
+++ backend/tests/Feature/Services/GetCpfCnpjTest.php
@@ -24,3 +24,25 @@ it('should be able to get data of supplier by valid cnpj', function () {
             ]
         ]);
 });
+
+it('should be able to return 400 response for invalid cnpj', function () {
+    $cnpj = '1111111111';
+
+    Http::fake([
+        'https://brasilapi.com.br/api/cnpj/v1/*' => Http::response('{
+            "message": "CNPJ 1111111 inválido.",
+            "type": "bad_request",
+            "name": "BadRequestError"
+        }', Response::HTTP_BAD_REQUEST),
+    ]);
+
+    $response = $this->get(route('suppliers.services', $cnpj));
+    $response->assertStatus(Response::HTTP_BAD_REQUEST)
+        ->assertJson([
+            [
+                'message' => 'CNPJ 1111111 inválido.',
+                'type' => 'bad_request',
+                'name' => 'BadRequestError',
+            ]
+        ]);
+});
