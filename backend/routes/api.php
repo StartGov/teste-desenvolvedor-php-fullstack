b842a461493875a3cd331c9ecbb1d7a83714efcf
Ivo <ivo_vilar@hotmail.com>
2024-02-03 13:14:20 -0300

    backend: fixing routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Route;
 // region Suppliers
 
 Route::get('/suppliers',Supplier\IndexController::class)->name('suppliers.index');
-Route::post('suppliers', Supplier\StoreController::class)->name('suppliers.store');
-Route::put('suppliers/{supplier}', Supplier\UpdateController::class)->name('suppliers.update');
-Route::delete('suppliers/{supplier}', Supplier\DeleteController::class)->name('suppliers.delete');
+Route::post('/suppliers', Supplier\StoreController::class)->name('suppliers.store');
+Route::put('/suppliers/{supplier}', Supplier\UpdateController::class)->name('suppliers.update');
+Route::delete('/suppliers/{supplier}', Supplier\DeleteController::class)->name('suppliers.delete');
 // endregion
 
 
