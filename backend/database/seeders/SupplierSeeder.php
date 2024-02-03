4c1c10f6b2ca4aae44c71d4d64bdf233ff47cf9c
Ivo <ivo_vilar@hotmail.com>
2024-02-03 12:45:20 -0300

    backend: adding seeder

--- backend/database/seeders/SupplierSeeder.php
+++ backend/database/seeders/SupplierSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Supplier;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class SupplierSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Supplier::factory()->count(20);
     }
 }
