dc9f5ce126ceb711bb5e5713fc09ea826f72a5ba
Ivo <ivo_vilar@hotmail.com>
2024-02-04 12:42:23 -0300

    frontend: adding sort, search

--- frontend/src/components/Supplier.vue
+++ frontend/src/components/Supplier.vue
@@ -1,27 +1,79 @@
 <template>
   <div>
-    <b-table
-      :busy="isBusy"
-      responsive
-      :items="suppliers"
-      :fields="fields"
-      :per-page="0"
-      :current-page="currentPage"
-    >
-      <template #table-busy>
-        <div class="text-center text-danger my-2">
-          <b-spinner class="align-middle"></b-spinner>
-          <strong>Loading...</strong>
-        </div>
-      </template>
-    </b-table>
-    <b-pagination
-      v-model="currentPage"
-      :total-rows="totalRows"
-      :per-page="perPage"
-      @input="pageChanged"
-      align="center"
-    />
+    <b-container fluid>
+      <b-row>
+        <b-row align-h="start">
+          <b-col cols="2">
+            <b-form-group
+                label="Filter"
+                label-for="filter-input"
+                label-cols-sm="3"
+                label-align-sm="right"
+                label-size="sm"
+                class="mb-0"
+            >
+              <b-input-group size="sm">
+                <b-form-input
+                    id="filter-input"
+                    v-model="filter"
+                    type="search"
+                    placeholder="Type to Search"
+                ></b-form-input>
+
+                <b-input-group-append>
+                  <b-button
+                      variant="danger"
+                      :disabled="!filter"
+                      @click="filter = ''">
+                    Clear
+                  </b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+          <b-col cols="2">
+            <b-form-group
+                label="Initial sort"
+                label-for="initial-sort-select"
+                label-cols-sm="3"
+                label-align-sm="right"
+                label-size="sm"
+                class="mb-0"
+            >
+              <b-form-select
+                  class="mt-2"
+                  id="initial-sort-select"
+                  v-model="sortDirection"
+                  :options="['asc', 'desc', 'last']"
+                  size="sm"
+              ></b-form-select>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-row>
+      <b-table
+        :busy="isBusy"
+        responsive
+        :items="suppliers"
+        :fields="fields"
+        :per-page="0"
+        :current-page="currentPage"
+      >
+        <template #table-busy>
+          <div class="text-center text-danger my-2">
+            <b-spinner class="align-middle"></b-spinner>
+            <strong>Loading...</strong>
+          </div>
+        </template>
+      </b-table>
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="totalRows"
+        :per-page="perPage"
+        @input="pageChanged"
+        align="center"
+      />
+    </b-container>
   </div>
 </template>
 
@@ -30,12 +82,28 @@
 import {
   BTable,
   BPagination,
+  BSpinner,
+  BCol,
+  BRow,
+  BFormGroup,
+  BFormInput,
+  BInputGroup,
+  BInputGroupAppend,
+  BButton,
 } from 'bootstrap-vue'
 import axios from '../axios'
 export default {
   components: {
     BTable,
     BPagination,
+    BSpinner,
+    BCol,
+    BRow,
+    BFormGroup,
+    BFormInput,
+    BInputGroup,
+    BInputGroupAppend,
+    BButton,
   },
   data() {
     return {
@@ -48,6 +116,20 @@ export default {
       currentPage: 1,
       totalRows: 0,
       isBusy: true,
+      sortBy: '',
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+      filterOn: [],
+    }
+  },
+  computed: {
+    sortOptions() {
+      return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
     }
   },
   created() {
