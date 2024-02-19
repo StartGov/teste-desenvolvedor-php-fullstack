856889bcf8bda2dd142a7b679c2adcd9a1b1883c
jordan-sousa <jordan.sousajc@gmail.com>
2024-02-19 15:22:20 -0300

    criando as migrations

---
+++ backend/database/migrations/2024_02_19_142611_create_supplier.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->id();
+            $table->string('social_reason');
+            $table->string('fantasy name');
+            $table->string('contact');
+            $table->string('cnpj');
+            
+            $table->foreign('addres_id')->references('id')->on('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier');
+    }
+};
